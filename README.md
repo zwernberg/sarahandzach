7d21b5a8f3b86c18dd36de1d8824f3f360608d7f
Zach Wernberg <zawerny@gmail.com>
2018-01-01 19:55:03 -0600

    add express to serve locally

---
+++ README.md
@@ -0,0 +1,8 @@
+# Install
+
+`npm install`
+
+# Serving Locally
+`node server.js`
+
+*visit http://localhost:3000*
---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+var path    = require("path");
+app.set('port', 3000);
+app.use(express.static(path.join(__dirname, '/')));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
+
+console.log("Running at Port 3000");
